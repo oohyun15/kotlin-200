35d19c668c5ee34804a84bb9ff1df9e8fcd13cf3
oohyun15 <sakiss4774@gmail.com>
2023-12-25 16:14:41 +0900

    :sparkles: 007

---
+++ src/007.kt
@@ -0,0 +1,11 @@
+// bitwise operator
+fun main(args: Array<String>): Unit
+{
+    println(15 and 7) // 15 & 7
+    println(5 or 2) // 5 | 2
+    println(15 xor 5) // 15 ^ 5
+    println(32767.inv()) // -32767
+    println(1 shl 3) // 1 << 3
+    println(8 shr 1) // 8 >> 1
+    println(-17 ushr 2) // -17 >>> 2 unsigned shift
+}
