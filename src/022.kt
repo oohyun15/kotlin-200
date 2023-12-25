b91344494f28da54f9241a1f872feefbbc1ea8e1
oohyun15 <sakiss4774@gmail.com>
2023-12-25 17:28:51 +0900

    :sparkles: 022

---
+++ src/022.kt
@@ -0,0 +1,16 @@
+// if expression
+fun main(args: Array<String>): Unit
+{
+    val value: Int = if (10 > 5)
+    {
+        println("10은 5보다 크다.")
+        10
+    }
+    else
+    {
+        println("10은 5보다 크지 않다.")
+        5
+    }
+
+    println(value)
+}
