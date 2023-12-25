c91547d9762fa0168c3b51837dec4618392fb270
oohyun15 <sakiss4774@gmail.com>
2023-12-25 17:41:01 +0900

    :sparkles: 027

---
+++ src/027.kt
@@ -0,0 +1,12 @@
+// continue
+fun main(args: Array<String>): Unit
+{
+    var i = 0
+    while (i < 10)
+    {
+        i += 1
+        if (i % 2 == 0)
+            continue
+        println(i) // print odd numbers
+    }
+}
