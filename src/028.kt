8182f9dca03cb7000141c5a442e60e502687b40d
oohyun15 <sakiss4774@gmail.com>
2023-12-25 17:42:12 +0900

    :sparkles: 028

---
+++ src/028.kt
@@ -0,0 +1,12 @@
+// break
+fun main(args: Array<String>): Unit
+{
+    var i = 0
+    while (true)
+    {
+        i += 1
+        if (i >= 5)
+            break
+        print(i) // print 1234
+    }
+}
