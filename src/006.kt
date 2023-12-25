8a94a4ebc80617f5847b1adaef3307cb03f9ce48
oohyun15 <sakiss4774@gmail.com>
2023-12-25 16:09:20 +0900

    :sparkles: 006

---
+++ src/006.kt
@@ -0,0 +1,10 @@
+// increment & decrement operator
+fun main(args: Array<String>): Unit
+{
+    var a = 10
+    var b = 5
+
+    println(a++ + b) // 15
+    println(a) // 11
+    println(-b) // -5
+}
