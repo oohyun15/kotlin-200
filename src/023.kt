d570f235739125f1f84af455ef7bf287677817ff
oohyun15 <sakiss4774@gmail.com>
2023-12-25 17:32:24 +0900

    :sparkles: 023

---
+++ src/023.kt
@@ -0,0 +1,16 @@
+// when
+fun main(args: Array<String>): Unit
+{
+    val score = 64
+
+    when (score / 10) // 6
+    {
+        6 -> { println('D') }
+        7 -> { println('C') }
+        8 -> { println('B') }
+        9, 10 -> { println('A') }
+        else -> { println('F') }
+    }
+
+    println("test")
+}
