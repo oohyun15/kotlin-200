02b9a1a8ad6348449457253273a3e8a5c149eaba
oohyun15 <sakiss4774@gmail.com>
2023-12-25 16:44:30 +0900

    :sparkles: 011

---
+++ src/011.kt
@@ -0,0 +1,14 @@
+// string
+fun main(args: Array<String>): Unit
+{
+    var str: String = "Hello"
+    println(str)
+
+    str = str + "\nKotlin!"
+    println(str)
+
+    println(str[8])
+
+    val num = 10 * 5 + 3
+    println(str + num) // implicit string conversion
+}
