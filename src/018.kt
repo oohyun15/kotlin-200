b0efb77deab461017e1aa89aef8d4249743c7acb
oohyun15 <sakiss4774@gmail.com>
2023-12-25 17:17:43 +0900

    :sparkles: 018

---
+++ src/018.kt
@@ -0,0 +1,12 @@
+// logical operator
+fun main(args: Array<String>): Unit
+{
+    val a = 15
+    val b = 17
+
+    var bool: Boolean = (a - b < a + b) && (a == 15)
+    println(bool) // true && true = true
+
+    bool = !((a + b) > (a * 3) || (b - a) > 0)
+    println(bool) // !(true || true) = !true = false
+}
