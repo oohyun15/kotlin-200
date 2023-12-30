6d6981a80cffe6ce8f1140c2e78f5b3818d48810
oohyun15 <sakiss4774@gmail.com>
2023-12-30 16:57:18 +0900

    :sparkles: 038

---
+++ src/038.kt
@@ -0,0 +1,22 @@
+// duplicated local variable between another functions
+
+fun main(args: Array<String>)
+{
+    val a = 52
+    println(a)
+
+    printA()
+    printA2()
+}
+
+fun printA()
+{
+    val a = 17
+    println(a)
+}
+
+fun printA2()
+{
+    val a = 120
+    println(a)
+}
