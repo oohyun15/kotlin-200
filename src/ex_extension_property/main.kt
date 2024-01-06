6e6e0b29ef435fc0ec108393736b81f9371290ba
oohyun15 <sakiss4774@gmail.com>
2024-01-06 19:41:32 +0900

    :sparkles: 087

---
+++ src/ex_extension_property/main.kt
@@ -0,0 +1,10 @@
+package ex_extension_property
+
+val String.isLarge: Boolean
+    get() = this.length >= 10
+
+fun main(args: Array<String>)
+{
+    println("1234567890".isLarge) // true
+    println("500원".isLarge) // false
+}
